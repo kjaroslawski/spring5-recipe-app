4b0f94987d4c2f093853049c4d998cbe3f99e411
Gustaw <kgj.mails@gmail.com>
2019-02-03 19:24:55 +0100

    Ingredient controller, list of ingredients.

---
+++ src/test/java/pl/qamar/controllers/IngredientControllerTest.java
@@ -0,0 +1,52 @@
+package pl.qamar.controllers;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import pl.qamar.commands.RecipeCommand;
+import pl.qamar.services.RecipeService;
+
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+public class IngredientControllerTest {
+
+    @Mock
+    RecipeService recipeService;
+
+    IngredientController ingredientController;
+
+    MockMvc mockMvc;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+        ingredientController = new IngredientController(recipeService);
+        mockMvc = MockMvcBuilders.standaloneSetup(ingredientController).build();
+    }
+
+    @Test
+    public void testListIngredients() throws Exception {
+        //Given
+        RecipeCommand recipeCommand = new RecipeCommand();
+        when(recipeService.findRecipeCommandById(anyLong())).thenReturn(recipeCommand);
+
+        //When
+        mockMvc.perform(get("/recipe/1/ingredients"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("recipe/ingredient/list"))
+                .andExpect(model().attributeExists("recipe"));
+
+        //Then
+        verify(recipeService, times(1)).findRecipeCommandById(anyLong());
+    }
+}
